0715a21aed32904da5cfa64d532e994779553925
omar3adwy <3adawy42@gmail.com>
2023-11-28 18:44:58 -0800

    fint the last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ lastdigit = abs(number) % 10
 if number < 0:
     lastdigit = -(lastdigit)
 thestring = "Last digit of {} is {}".format(number, lastdigit)
-if lastdigit> 5:
+if lastdigit > 5:
     print(f"{thestring} and is greater than 5")
 elif lastdigit == 0:
     print(f"{thestring} and is 0")
