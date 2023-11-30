5e767bfdf77f956bf6963701b34fce31d2a13a48
omar3adwy <3adawy42@gmail.com>
2023-11-29 18:17:03 -0800

    unction that insert a number intosorted list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -18,7 +18,11 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *temp = NULL;
 
 	if (!head)
-		return (NULL);
+		return(NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return(NULL);
 
 	new->n = number;
 	new->next = NULL;
