99c1ad9297656391024dccf4d5a5035ad35a5f99
omar3adwy <3adawy42@gmail.com>
2023-12-04 17:41:25 -0800

    lists

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     if my_list is None:
         return None
     listdivs = []
-    for i in my_list:9
+    for i in my_list:
         if (i % 2) == 0:
             listdivs.append(True)
         else:
